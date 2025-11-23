acbeac50c249d243005e38db2b7fe9e5814dcac2
mfarahmand-deh <mfarahmand-dehghanpo@myseneca.ca>
2025-11-23 11:33:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+# Terraform settings
 terraform {
   required_providers {
     aws = {
@@ -23,60 +21,32 @@ terraform {
   }
 }
 
+# AWS provider
 provider "aws" {
   region = "us-west-2"
 }
 
+# Random name generator for security group
 resource "random_pet" "sg" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
+# Security group only
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name        = "${random_pet.sg.id}-sg"
+  description = "Staging web server security group"
+
+  # Ingress: only allow your specific IP
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.0.113.10/32"] # replace with your IP
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
+  # Egress: allow all (or restrict as needed)
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # you can restrict if needed
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
